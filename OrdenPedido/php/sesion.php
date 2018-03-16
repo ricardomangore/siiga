0319bd9fa55c6ff523df8bc4913c273ef9d2153a
Ricardo Mangoré <ricardomangore@gmail.com>
2018-03-16 12:52:54 -0600

    Se excluye el directorio del sub-sistema OrdenPedido del repositorio del SIIGA

--- OrdenPedido/php/sesion.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-
-//Evaluo que la sesion continue verificando una de las variables creadas en control.php, cuando esta ya no coincida con su valor inicial se redirije al archivo de salir.php
-if(!$_SESSION["autentificado"]){
-	header("Location: salir.php");
-}
-?>
