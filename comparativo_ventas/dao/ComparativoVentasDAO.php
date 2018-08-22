c79fd69100e32f2a17551d3ce6599056bad3668a
Mangore <ricardomangore@gmail.com>
2018-08-22 11:08:03 -0500

    Se actualizaron los archivos ComaprativoVentasDAO.php y Connect.php

--- comparativo_ventas/dao/ComparativoVentasDAO.php
+++ comparativo_ventas/dao/ComparativoVentasDAO.php
@@ -4,16 +4,21 @@ require_once('/../includes/Connect.php');
 require_once('/../pojos/PostPago.php');
 
 class ComparativoVentasDAO extends Connect{
-
-	public function nombrePdvCompareByFolioImei($postPago){
+	/**
+	 * Determina si ounb objeto PostPago es igual a un registro de ventas dentro del SIIGA
+	 * a partir del folio, IMEI y Nombre del Punto de Venta registrado en ATT
+	 * @param <Object> PostPago
+	 * @return <boolean>
+	 */
+	public function comparePostPagoByNombrePDV($postPago){
 		$returnValue = FALSE;
 		if(isset($postPago) && is_a($postPago,'PostPago')){
 			$folio = $postPago->getFolio();
 			$imei = $postPago->getImei();
-			$sqlStr="SELECT HFolios.Folio FROM HFolios INNER JOIN LFolios ON HFolios.Folio=LFolios.Folio INNER JOIN PuntosATT ON HFolios.PuntoventaId=PuntosATT.PuntoVentaId WHERE HFolios.Folio LIKE ? AND LFolios.Serie=?";
+			$nombrePdv = $postPago->getNombrePdvUnico();
+			$sqlStr="SELECT HFolios.Folio FROM HFolios INNER JOIN LFolios ON HFolios.Folio=LFolios.Folio INNER JOIN PuntosATT ON HFolios.PuntoventaId=PuntosATT.PuntoVentaId WHERE HFolios.Folio LIKE ? AND LFolios.Serie=? AND PuntosATT.NombreATT=?";
 			if($prepare = $this->getLink()->prepare($sqlStr)){
-				$folioLike = "'%".$folio."%'";
-				$prepare->bind_param('ss',$folioLike,$imei);
+				$prepare->bind_param('sss',$folio,$imei,$nombrePdv);
 				$prepare->execute();
 				$prepare->bind_result($param);
 				$prepare->fetch();
@@ -31,11 +36,65 @@ class ComparativoVentasDAO extends Connect{
 		return $returnValue;
 	}
 
-	public function nombrePdvCompareByFolioSim($postPago){
+	/*public function comparePostPagoBySim($postPago){
 		$returnValue = FALSE;
 		if(isset($postPago) && is_a($postPago,'PostPago')){
 			$folio = $postPago->getFolio();
 			$sim = $posPago->getSim();
+
+		}
+		return $returnValue;
+	}*/
+
+	public function comparePostPagoByTipoVenta($postPago){
+		$returnValue = FALSE;
+		if(isset($postPago) && is_a($postPago,'PsotPago')){
+			$folio = $postPago->getFolio();
+			$imei = $postPago->getImei();
+			$tipoVenta = $postPago->getTipoVenta();
+			$sqlStr= 'SELECT HFolios.Folio FROM HFolios INNER JOIN LFolios ON HFolios.Folio=LFolios.Folio INNER JOIN TiposContratacion ON HFolios.TipoContratacionId=TiposContratacion.TipoContratacionId WHERE HFolios.Folio LIKE ? AND LFolios.Serie = ? aND TiposContratacion.Tipocontratacion LIKE ?';
+			if($prepare = $this->getLink()->prepare($sqlStr)){
+				$prepare->bind_param('sss',$folio,$imei,$tipoVenta);
+				$prepare->execute();
+				$prepare->bind_result($param);
+				$prepare->fetch();
+
+				var_dump($param);
+
+				$prepare->close();
+				if($param != ''){
+					$returnValue = TRUE;
+				}
+			}else{
+				throw new Exception("No se puedo preparar la consulta");
+			}
+
+		}
+		return $returnValue;
+	}
+
+	public function comparePostPagoByNombreEjecutivo($postPago){
+		$returnValue = FALSE;
+		if(isset($postPago) && is_a($postPago,'PsotPago')){
+			$folio = $postPago->getFolio();
+			$imei = $postPago->getImei();
+			$tipoVenta = $postPago->getNombreEjecutivoUnico();
+			$sqlStr= "SELECT Empleados.Nombre FROM Usuarios INNER JOIN Empleados ON Empleados.EmpleadoId=Usuarios.EmpleadoId WHERE Usuarios.UsuarioId=(SELECT HFolios.UsuarioId FROM HFolios INNER JOIN LFolios ON HFolios.Folio=LFolios.Folio INNER JOIN Usuarios ON HFolios.UsuarioId=Usuarios.UsuarioId INNER JOIN Empleados ON Usuarios.EmpleadoId=Empleados.EmpleadoId WHERE HFolios.Folio LIKE ? AND LFolios.Serie = ? ) AND CONCAT(Empleados.Nombre,' ',Empleados.Paterno,' ',Empleados.Materno) LIKE ?";
+			if($prepare = $this->getLink()->prepare($sqlStr)){
+				$prepare->bind_param('sss',$folio,$imei,$tipoVenta);
+				$prepare->execute();
+				$prepare->bind_result($param);
+				$prepare->fetch();
+
+				var_dump($param);
+
+				$prepare->close();
+				if($param != ''){
+					$returnValue = TRUE;
+				}
+			}else{
+				throw new Exception("No se puedo preparar la consulta");
+			}
 
 		}
 		return $returnValue;
--- comparativo_ventas/includes/Connect.php
+++ comparativo_ventas/includes/Connect.php
@@ -5,7 +5,7 @@ class Connect{
 	private $link;
 
 	public function __construct () {
-		$this->link =  new mysqli("localhost", "root", "", "siiga");
+		$this->link =  new mysqli("solucell.com.mx", "solucell_tester", "1nt3rm3zz0$", "solucell_siigatest");
 		if (mysqli_connect_errno()) {
 			throw new Exception('Error de conexión a base de datos: ' . mysqli_connect_error() );
 		}
