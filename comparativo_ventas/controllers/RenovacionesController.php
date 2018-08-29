f7eec5d336e26f37947f4940b28aee38c4ab748d
Mangore <ricardomangore@gmail.com>
2018-08-29 12:35:18 -0500

    Se modifica la letra mayuscula al inicio de las llamadas de los path de Renovaciones Controlles cambiando Comparativo_ventas por comparativo_ventas y evitar el warning emitido por el error_log del servidor siigatester

--- comparativo_ventas/controllers/RenovacionesController.php
+++ comparativo_ventas/controllers/RenovacionesController.php
@@ -4,8 +4,8 @@ include_once("comparativo_ventas/dao/RenovacionesDAO.php");
 include_once("comparativo_ventas/dao/ComparativoVentasDAO.php");
 include_once("comparativo_ventas/dao/TiposDiferenciasDAO.php");
 include_once("comparativo_ventas/pojos/Diferencias.php");
-include_once("Comparativo_ventas/pojos/ViewRenovaciones.php");
-include_once("Comparativo_ventas/includes/ToolsComparativoVentas.php");
+include_once("comparativo_ventas/pojos/ViewRenovaciones.php");
+include_once("comparativo_ventas/includes/ToolsComparativoVentas.php");
 /**
  * 
  */
